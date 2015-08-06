c730ffca4d2337df22fbcc2605e2ca749ece1a7b
Aditya Ramesh <_@adityaramesh.com>
2015-08-06 00:45:11 -0400

    Deleted cr function, and renamed scr template alias to cr.

--- include/ndmath/range/range_builder.hpp
+++ include/ndmath/range/range_builder.hpp
@@ -32,33 +32,12 @@ public:
 	}
 };
 
-class const_range_builder final
-{
-public:
-	template <class A, class B>
-	CC_ALWAYS_INLINE constexpr
-	auto operator()(const A& a, const B& b)
-	const noexcept
-	{
-		return make_range(c_index(a), c_index(b));
-	}
-
-	template <class A, class B, class S>
-	CC_ALWAYS_INLINE constexpr
-	auto operator()(const A& a, const B& b, const S& s)
-	const noexcept
-	{
-		return make_range(c_index(a), c_index(b), c_index(s));
-	}
-};
-
 namespace tokens {
 
 static constexpr auto r = range_builder{};
-static constexpr auto cr = const_range_builder{};
 
 template <unsigned A, unsigned B, unsigned S = 1>
-static constexpr auto scr = r(sc_coord<A>, sc_coord<B>, sc_coord<S>);
+static constexpr auto cr = r(sc_coord<A>, sc_coord<B>, sc_coord<S>);
 
 }
 
